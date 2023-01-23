8a739574ec4c64e80e78c834710b6dae4971cc57
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-01-23 00:28:23 -0500

    new sections

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
-const hamburger = document.querySelector('#hamburger')
-const menu = document.querySelector('#menu')
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
 
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('hidden')
-    hamburger.classList.toggle('bg-white')
-})
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  hamburger.classList.toggle("bg-white");
+});
 
+const hero = document.getElementById("hero");
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("change", (event) => {
+  if (event.target.checked) {
+    hero.classList.remove("light");
+    hero.classList.add("dark");
+  } else {
+    hero.classList.remove("dark");
+    hero.classList.add("light");
+  }
+});
