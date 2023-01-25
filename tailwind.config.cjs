d5ebefdae2875d39b9fdb7e3c704d1e58b0e9ec4
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-01-25 18:26:08 -0500

    pages op

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#274c77",
+
+          secondary: "#6096ba",
+
+          accent: "#a3cef1",
+
+          neutral: "#8b8c89",
+
+          "base-100": "#e7ecef",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("@tailwindcss/forms")],
+  plugins: [require("daisyui")],
 };
