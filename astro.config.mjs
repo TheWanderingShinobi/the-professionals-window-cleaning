7a478f7f4b58f2e422a1d4d16fe1aa9b3eba2ce2
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-01-26 12:47:52 -0500

    netlify added

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()]
+  integrations: [tailwind(), compress()],
+  output: "server",
+  adapter: netlify()
 });
