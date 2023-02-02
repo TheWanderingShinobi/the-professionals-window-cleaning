b2ffdd7ad98bdafdd81cc390f0fa84e9a9136818
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-02-01 19:50:43 -0500

    footer added

--- src/app.js
+++ src/app.js
@@ -3,18 +3,4 @@ const menu = document.querySelector("#menu");
 
 hamburger.addEventListener("click", () => {
 	menu.classList.toggle("hidden");
-	hamburger.classList.toggle("bg-white");
-});
-
-const hero = document.getElementById("hero");
-const toggle = document.getElementById("toggle");
-
-toggle.addEventListener("change", (event) => {
-	if (event.target.checked) {
-		hero.classList.remove("light");
-		hero.classList.add("dark");
-	} else {
-		hero.classList.remove("dark");
-		hero.classList.add("light");
-	}
 });
