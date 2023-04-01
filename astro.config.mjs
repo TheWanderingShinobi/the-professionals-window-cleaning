054c6084928e0110220ce2647f2bf849c338592d
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-04-01 13:18:05 -0400

    seo and contact id updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import compress from "astro-compress";
 
 // https://astro.build/config
@@ -23,45 +21,14 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 
 // https://astro.build/config
+import critters from "astro-critters";
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://theprofessionalswindowcleaning.com",
-	integrations: [
-		tailwind(),
-		compress(),
-		robotsTxt(),
-		sitemap({
-			customPages: [
-				"https://theprofessionalswindowcleaning.com/",
-				"https://theprofessionalswindowcleaning.com/about",
-				"https://theprofessionalswindowcleaning.com/contact",
-				"https://theprofessionalswindowcleaning.com/services",
-				"https://theprofessionalswindowcleaning.com/privacy-policy",
-				"https://theprofessionalswindowcleaning.com/terms-of-service",
-				"https://theprofessionalswindowcleaning.com/cookie-policy",
-				"https://theprofessionalswindowcleaning.com/404",
-				"https://theprofessionalswindowcleaning.com/projects",
-				"https://theprofessionalswindowcleaning.com/brampton-window-cleaning",
-				"https://theprofessionalswindowcleaning.com/mississauga-window-cleaning",
-				"https://theprofessionalswindowcleaning.com/scarborough-window-cleaning",
-				"https://theprofessionalswindowcleaning.com/eavestrough-cleaning",
-				"https://theprofessionalswindowcleaning.com/eavestrough-cleaning-ajax",
-				"https://theprofessionalswindowcleaning.com/eavestrough-cleaning-pickering",
-				"https://theprofessionalswindowcleaning.com/eavestrough-cleaning-richmond-hill",
-				"https://theprofessionalswindowcleaning.com/eavestrough-cleaning-scarborough",
-				"https://theprofessionalswindowcleaning.com/eavestrough-gutter-repair",
-				"https://theprofessionalswindowcleaning.com/etobicoke-eavestrough-cleaning",
-				"https://theprofessionalswindowcleaning.com/north-york-window-cleaning",
-				"https://theprofessionalswindowcleaning.com/power-washing",
-				"https://theprofessionalswindowcleaning.com/toronto-window-cleaning",
-				"https://theprofessionalswindowcleaning.com/window-cleaning",
-				"https://theprofessionalswindowcleaning.com/window-cleaning-etobicoke",
-				"https://theprofessionalswindowcleaning.com/window-cleaning-markham",
-				"https://theprofessionalswindowcleaning.com/window-cleaning-thornhill",
-				"https://theprofessionalswindowcleaning.com/window-cleaning-vaughan",
-			],
-		}),
-		rome(),
-	],
-	output: "server",
-	adapter: cloudflare(),
+  site: "https://theprofessionalswindowcleaning.com",
+  integrations: [tailwind(), compress(), robotsTxt(), sitemap({
+    customPages: ["https://theprofessionalswindowcleaning.com/", "https://theprofessionalswindowcleaning.com/about", "https://theprofessionalswindowcleaning.com/contact", "https://theprofessionalswindowcleaning.com/services", "https://theprofessionalswindowcleaning.com/privacy-policy", "https://theprofessionalswindowcleaning.com/terms-of-service", "https://theprofessionalswindowcleaning.com/cookie-policy", "https://theprofessionalswindowcleaning.com/404", "https://theprofessionalswindowcleaning.com/projects", "https://theprofessionalswindowcleaning.com/brampton-window-cleaning", "https://theprofessionalswindowcleaning.com/mississauga-window-cleaning", "https://theprofessionalswindowcleaning.com/scarborough-window-cleaning", "https://theprofessionalswindowcleaning.com/eavestrough-cleaning", "https://theprofessionalswindowcleaning.com/eavestrough-cleaning-ajax", "https://theprofessionalswindowcleaning.com/eavestrough-cleaning-pickering", "https://theprofessionalswindowcleaning.com/eavestrough-cleaning-richmond-hill", "https://theprofessionalswindowcleaning.com/eavestrough-cleaning-scarborough", "https://theprofessionalswindowcleaning.com/eavestrough-gutter-repair", "https://theprofessionalswindowcleaning.com/etobicoke-eavestrough-cleaning", "https://theprofessionalswindowcleaning.com/north-york-window-cleaning", "https://theprofessionalswindowcleaning.com/power-washing", "https://theprofessionalswindowcleaning.com/toronto-window-cleaning", "https://theprofessionalswindowcleaning.com/window-cleaning", "https://theprofessionalswindowcleaning.com/window-cleaning-etobicoke", "https://theprofessionalswindowcleaning.com/window-cleaning-markham", "https://theprofessionalswindowcleaning.com/window-cleaning-thornhill", "https://theprofessionalswindowcleaning.com/window-cleaning-vaughan"]
+  }), rome(), critters()],
+  output: "server",
+  adapter: cloudflare()
 });
